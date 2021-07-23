17cfdb906b54bd6f1c1d63ab2bff17b873d88430
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2021-07-22 21:20:05 -0700

    Navbar is added and working except 'ALL' menu tab

--- restaurant/src/component/Basics/Restaurant.js
+++ restaurant/src/component/Basics/Restaurant.js
@@ -7,8 +7,27 @@ import MenuCard from "./menuCard";
 const Restaurant = () => {
 
   const [menuData, setMenuData] = useState(Menu);
+  const filterItem = ( category ) => {
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
   return (
      <>
+        <nav className="navbar">
+          <div className="btn-group">
+            <button className="btn-group__item" onClick={() => filterItem("breakfast")}>Breakfast</button>
+            <button className="btn-group__item" onClick={() => filterItem("lunch")}>lunch</button>
+            <button className="btn-group__item" onClick={() => filterItem("evening")}>Evening</button>
+            <button className="btn-group__item" onClick={() => filterItem("dinner")}>Dinner</button>
+            <button className="btn-group__item">All</button>
+          </div>
+        </nav>
+
         <MenuCard menuData={menuData} />
      </>
   )
